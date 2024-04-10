e9208e3efe1ed517b301545fcad2905ca315fd8e
Priya.Sharma <priyasharma2702@gmail.com>
2024-04-10 17:28:41 +0530

    changed base in vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://priyasharma27021992.github.io/miss-techie/",
   plugins: [react()],
 });
