e794ea66c61fe2ccf541b943981ec6828098754c
Priya.Sharma <priyasharma2702@gmail.com>
2024-04-10 17:11:38 +0530

    first version

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./pages/home";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
