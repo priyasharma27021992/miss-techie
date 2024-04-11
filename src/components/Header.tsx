e1634895e6c6b13ceeba6aedbb018be6b3c64ae9
Priya.Sharma <priyasharma2702@gmail.com>
2024-04-11 18:01:19 +0530

    corrected mobile menu and added resume

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import PriyaResume from "../assets/priya-sharma-resume-2024.pdf";
+
 const Header = () => {
+  const [openMobileMenu, setOpenMobileMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMobileMenu(!openMobileMenu);
+  };
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Miss Techie
+            Miss Techie In-Progress
           </span>
         </a>
         <button
@@ -12,7 +20,8 @@ const Header = () => {
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
-          aria-expanded="false"
+          aria-expanded={`${openMobileMenu ? true : false}`}
+          onClick={toggleMenu}
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -31,8 +40,19 @@ const Header = () => {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div
+          className={`${
+            openMobileMenu ? "block w-full" : "hidden"
+          } md:block md:w-auto relative`}
+          id="navbar-default"
+        >
+          <ul
+            className={`font-medium flex flex-col items-baseline p-4 md:p-0 mt-4 border border-gray-100 rounded-lg ${
+              openMobileMenu &&
+              "absolute right-0 w-full items-center md:relative md:w-auto md:items-start"
+            }
+          bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700`}
+          >
             <li>
               <a
                 href="#"
@@ -63,15 +83,25 @@ const Header = () => {
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Pricing
+                Contact
               </a>
             </li>
             <li>
               <a
-                href="#"
+                href={PriyaResume}
+                download
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Contact
+                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                  <svg
+                    className="fill-current w-4 h-4 mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                  </svg>
+                  <span>Download Resume</span>
+                </button>
               </a>
             </li>
           </ul>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,37 +1,56 @@
 import PortfolioImg from "../assets/portfolio-priya.jpg";
+import GithubSvg from "../assets/github.svg";
+import LinkedInSvg from "../assets/linked-in.svg";
+
+import "../styles/pure-particle.scss";
 const Home = () => {
   return (
-    <div className="h-screen bg-white dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-between rounded-lg m-auto h-full max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 md:flex-row bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
-        <div className="flex flex-col p-20">
-          <p className="mb-4">Hi! I am Priya Sharma</p>
-          <h1 className="mb-2 text-2xl font-bold text-gray-900 dark:text-white">
-            I will help you build a beautiful Frontend
-          </h1>
-          <p className="font-light mb-4">
-            I am a frontend developer with overall experience of 10 years and
-            releavant of 6 years on building frontend end. I ahve worked upon
-            mainly reactjs, nextjs, vanilla js, tailwind css, material-ui,
-            styling etc.
-          </p>
+    <>
+      {Array.from(Array(100).keys()).map(() => (
+        <div className="circle-container">
+          <div className="circle"></div>
+        </div>
+      ))}
+      <div className="h-screen bg-white dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-between rounded-lg m-auto h-full max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 md:flex-row bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+          <div className="flex flex-col p-5 md:p-10 max-w-2xl">
+            <p className="mb-4">Hi! I am Priya Sharma</p>
+            <h1 className="mb-2 text-2xl font-bold text-gray-900 dark:text-white">
+              I will help you build a beautiful Frontend
+            </h1>
+            <p className="font-light mb-4">
+              I am a frontend developer with overall experience of 10 years and
+              releavant of 6 years on building frontend end. I ahve worked upon
+              mainly reactjs, nextjs, vanilla js, tailwind css, material-ui,
+              styling etc.
+            </p>
+            <div>
+              <a
+                href=""
+                className="font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700"
+              >
+                Want to know more?
+              </a>
+              <div className="flex flex-row justify-start flex-wrap">
+                <img src={GithubSvg} alt="Github" className="m-3 w-10 h-10" />
+                <img
+                  src={LinkedInSvg}
+                  alt="LinkedIn"
+                  className="m-3 w-10 h-10"
+                />
+              </div>
+            </div>
+          </div>
           <div>
-            <a
-              href=""
-              className="font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700"
-            >
-              Want to know more?
-            </a>
+            <img
+              className="object-cover rounded-full shadow-2xl aspect-square w-96 h-96"
+              src={PortfolioImg}
+              alt="Priya Image"
+            />
           </div>
         </div>
-        <div className="">
-          <img
-            className="object-cover rounded-full shadow-2xl aspect-square"
-            src={PortfolioImg}
-            alt="Priya Image"
-          />
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
