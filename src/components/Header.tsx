5048476347e34478d87e52562b8fb1806b541495
Priya.Sharma <priyasharma2702@gmail.com>
2024-09-24 17:35:56 +0530

    added some style changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,112 +11,114 @@ const Header = () => {
     setOpenMobileMenu(!openMobileMenu);
   };
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link
-          to="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            className="rounded-t-lg max-w-20 hidden sm:block"
-            src="miss-techie-logo.jpg"
-            alt="Miss Techie logo"
-          />
-          <span className="self-center text-2xl whitespace-nowrap dark:text-white">
-            Miss Techie In-Progress
-          </span>
-        </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={`${openMobileMenu ? true : false}`}
-          onClick={toggleMenu}
-          onMouseLeave={toggleMenu}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
+    <header className="sticky top-0 w-full">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link
+            to="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
+            <img
+              className="rounded-t-lg max-w-20 hidden sm:block"
+              src="miss-techie-logo.jpg"
+              alt="Miss Techie logo"
             />
-          </svg>
-        </button>
-        <div
-          className={`${
-            openMobileMenu ? "block w-full" : "hidden"
-          } md:block md:w-auto relative`}
-          id="navbar-default"
-        >
-          <ul
-            className={`font-medium flex flex-col items-baseline p-4 md:p-0 mt-4 border border-gray-100 rounded-lg ${
-              openMobileMenu &&
-              "absolute right-0 w-full items-center md:relative md:w-auto"
-            }
-          bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700`}
+            <span className="self-center text-2xl whitespace-nowrap dark:text-white">
+              Miss Techie In-Progress
+            </span>
+          </Link>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded={`${openMobileMenu ? true : false}`}
+            onClick={toggleMenu}
+            onMouseLeave={toggleMenu}
           >
-            <li>
-              <Link
-                to="/"
-                className={`block py-2 px-3 text-xl ${
-                  pathname === "" ? "underline" : ""
-                } rounded md:bg-transparent md:p-0 dark:text-white hover:text-blue-700`}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`block py-2 px-3 text-xl rounded ${
-                  pathname === "projects" ? "underline" : ""
-                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-900 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
-                to="/projects"
-              >
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/contact"
-                className={`block py-2 px-3 text-xl  rounded ${
-                  pathname === "contact" ? "underline" : ""
-                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
-              >
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={PriyaResume}
-                download
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-              >
-                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold py-2 px-4 rounded-2xl inline-flex items-center">
-                  <svg
-                    className="fill-current w-4 h-4 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                  </svg>
-                  <span>Download Resume</span>
-                </button>
-              </Link>
-            </li>
-          </ul>
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div
+            className={`${
+              openMobileMenu ? "block w-full" : "hidden"
+            } md:block md:w-auto relative`}
+            id="navbar-default"
+          >
+            <ul
+              className={`font-medium flex flex-col items-baseline p-4 md:p-0 mt-4 border border-gray-100 rounded-lg ${
+                openMobileMenu &&
+                "absolute right-0 w-full items-center md:relative md:w-auto"
+              }
+          bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700`}
+            >
+              <li>
+                <Link
+                  to="/"
+                  className={`block py-2 px-3 text-xl ${
+                    pathname === "" ? "underline" : ""
+                  } rounded md:bg-transparent md:p-0 dark:text-white hover:text-blue-700`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`block py-2 px-3 text-xl rounded ${
+                    pathname === "projects" ? "underline" : ""
+                  } hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-900 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                  to="/projects"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/contact"
+                  className={`block py-2 px-3 text-xl  rounded ${
+                    pathname === "contact" ? "underline" : ""
+                  } hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                >
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={PriyaResume}
+                  download
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                >
+                  <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold py-2 px-4 rounded-2xl inline-flex items-center">
+                    <svg
+                      className="fill-current w-4 h-4 mr-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                    </svg>
+                    <span>Download Resume</span>
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,74 +7,82 @@ import SkillSetProgressBar from "../components/SkillSetProgressBar";
 
 const Home = () => {
   return (
-    <section className="w-screen h-[calc(100vh-5rem)]">
-      {Array.from(Array(100).keys()).map(() => (
-        <div className="circle-container">
-          <div className="circle"></div>
-        </div>
-      ))}
-      <div className="h-full w-full bg-white dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-between md:rounded-[3rem] m-auto lg:h-screen max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 md:flex-row border-2 bg-black text-white">
-          <div className="flex flex-col p-5 md:p-10 max-w-2xl">
-            <h1 className="mb-2 text-2xl font-bold animate-typing overflow-hidden whitespace-nowrap">
-              Hello!
-            </h1>
-            <p className="m-0 normal-case animate-typing overflow-hidden whitespace-nowrap">
-              I am Priya Sharma from Delhi(India)!
-            </p>
-            <p className="font-light mb-4 w-xl">
-              "With a decade-long journey in frontend development, I've honed my
-              skills over the past six years to become a specialist in this
-              field. My expertise lies in crafting dynamic and engaging user
-              interfaces using a diverse set of tools and technologies. I have
-              extensive experience working with ReactJS, NextJS, Vanilla
-              JavaScript, Tailwind CSS, Material-UI, and various styling
-              techniques. Whether it's building responsive layouts, implementing
-              intricate design elements, or optimizing performance, I thrive on
-              delivering solutions that seamlessly blend functionality with
-              aesthetics. My passion for frontend development drives me to stay
-              updated with the latest industry trends and best practices,
-              ensuring that I consistently deliver high-quality results that
-              exceed expectations."
-            </p>
-            <div className="">
-              <a
-                href=""
-                className="font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700"
-              >
-                Want to know more?
-              </a>
-              <div className="flex flex-row justify-start flex-wrap">
+    <main>
+      <section className="w-screen h-[calc(100vh-5rem)]">
+        {Array.from(Array(100).keys()).map(() => (
+          <div className="circle-container">
+            <div className="circle"></div>
+          </div>
+        ))}
+        <article className="h-full w-full bg-white dark:bg-gray-900">
+          <div className="flex flex-col items-center justify-between md:rounded-[3rem] m-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 md:flex-row border-2 bg-black text-white">
+            <div className="flex flex-col p-5 md:p-10 max-w-2xl">
+              <h1 className="mb-2 text-2xl font-bold animate-typing overflow-hidden whitespace-nowrap">
+                Hello!
+              </h1>
+              <p className="m-0 normal-case animate-typing overflow-hidden whitespace-nowrap">
+                I am Priya Sharma from Delhi(India)!
+              </p>
+              <p className="font-light mb-4 w-xl">
+                "With a decade-long journey in frontend development, I've honed
+                my skills over the past six years to become a specialist in this
+                field. My expertise lies in crafting dynamic and engaging user
+                interfaces using a diverse set of tools and technologies. I have
+                extensive experience working with ReactJS, NextJS, Vanilla
+                JavaScript, Tailwind CSS, Material-UI, and various styling
+                techniques. Whether it's building responsive layouts,
+                implementing intricate design elements, or optimizing
+                performance, I thrive on delivering solutions that seamlessly
+                blend functionality with aesthetics. My passion for frontend
+                development drives me to stay updated with the latest industry
+                trends and best practices, ensuring that I consistently deliver
+                high-quality results that exceed expectations."
+              </p>
+              <div className="">
                 <a
-                  href="https://github.com/priyasharma27021992"
-                  target="_blank"
+                  href=""
+                  className="font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700"
                 >
-                  <img src={GithubSvg} alt="Github" className="m-3 w-10 h-10" />
+                  Want to know more?
                 </a>
-                <a
-                  href="https://www.linkedin.com/in/priya-sharma-ab86ba46/"
-                  target="_blank"
-                >
-                  <img
-                    src={LinkedInSvg}
-                    alt="LinkedIn"
-                    className="m-3 w-10 h-10"
-                  />
-                </a>
+                <div className="flex flex-row justify-start flex-wrap">
+                  <a
+                    href="https://github.com/priyasharma27021992"
+                    target="_blank"
+                  >
+                    <img
+                      src={GithubSvg}
+                      alt="Github"
+                      className="m-3 w-10 h-10"
+                    />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/priya-sharma-ab86ba46/"
+                    target="_blank"
+                  >
+                    <img
+                      src={LinkedInSvg}
+                      alt="LinkedIn"
+                      className="m-3 w-10 h-10"
+                    />
+                  </a>
+                </div>
               </div>
+              <SkillSetProgressBar />
             </div>
-            <SkillSetProgressBar />
+            <aside>
+              <figure>
+                <img
+                  className="object-cover rounded-full shadow-2xl aspect-square w-96 h-96"
+                  src={PortfolioImg}
+                  alt="Priya Image"
+                />
+              </figure>
+            </aside>
           </div>
-          <div>
-            <img
-              className="object-cover rounded-full shadow-2xl aspect-square w-96 h-96"
-              src={PortfolioImg}
-              alt="Priya Image"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+        </article>
+      </section>
+    </main>
   );
 };
 
