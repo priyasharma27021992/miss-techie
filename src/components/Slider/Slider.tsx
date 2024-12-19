04c710480a4fb9618be0327376e33d0358de19fa
Priya.Sharma <priyasharma2702@gmail.com>
2024-12-19 23:35:30 +0530

    some code in slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,5 +1,17 @@
 import {} from "react";
 
-const Slider = () => {};
+const Slider = () => {
+  <section className="w-full flex justify-center absolute">
+    <div className="">
+      <button>left</button>
+      <div className="">
+        {[1, 2, 3, 4, 5, 6].map((item, index) => {
+          return <div className="w-[3000px] h-[300px] relative black"></div>;
+        })}
+      </div>
+      <button>right</button>
+    </div>
+  </section>;
+};
 
 export { Slider };
